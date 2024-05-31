797cdec9a1ee5617870f3092482b8b6ea28cfbfa
Majeed Godhrawala <majeedgodhrawala@gmail.com>
2024-05-31 13:32:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <MyButton />
+        <h2>
+          {user.name} <br />
+          {user.user_name}
+        </h2>
+        <ul>{list}</ul>
       </header>
     </div>
   );
 }
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function countButtonClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={countButtonClick}>Button Clicked {count}</button>;
+}
+
+const user = {
+  name: "Majeed",
+  user_name: "majeed@gmail.com",
+};
+
+const products = [
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+];
+
+const list = products.map((product) => (
+  <li key={product.id}>{product.title}</li>
+));
 
 export default App;
